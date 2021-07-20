5eb3c5dedb036a7252a3b9f88c268bb4aa502599
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-07-19 21:31:49 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React , { useState }from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+// import { AppLoading, Font} from 'expo';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+import producstReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import ShopNavigator from './navigation/ShopNavigator';
+
+const rootReducer = combineReducers({
+  products: producstReducer,
+  cart: cartReducer,
+  orders: ordersReducer
+});
+
+const store = createStore(rootReducer);
+
+const fetchFonts = async () => {
+  return await Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={ () => {
+          setFontLoaded(true);
+        }}
+        onError={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator></ShopNavigator>
+      </Provider>
+    );
+  }
+  
+}
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,75 @@
+import {  createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Platform } from "react-native";
+import React from  'react';
+
+import ProductsOverViewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
+import OrderScreen from "../screens/shop/OrderScreen";
+import UserProductScreen from "../screens/user/UserProductsScreen";
+import EditProductScreen from "../screens/user/EditProductScreen";
+import Colors from "../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const defaultNavigationOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ?  Colors.primary : ''
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+    },
+    heaserTintColor: Platform.OS === 'android' ?  'white' : Colors.primary
+}
+
+const ProductsNavigator = createStackNavigator({
+    ProductsOverview: ProductsOverViewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} size={23} color={drawerConfig.tintColor}/>
+        )
+    },
+    defaultNavigationOptions: defaultNavigationOptions
+});
+
+const OrdersNavigator = createStackNavigator({
+    Orders: OrderScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons name={Platform.OS === 'android' ? 'md-list' : 'ios-list'} size={23} color={drawerConfig.tintColor}/>
+        )
+    },
+    defaultNavigationOptions: defaultNavigationOptions
+});
+
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductScreen,
+    EditProduct: EditProductScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons name={Platform.OS === 'android' ? 'md-create' : 'ios-create'} size={23} color={drawerConfig.tintColor}/>
+        )
+    },
+    defaultNavigationOptions: defaultNavigationOptions
+});
+
+const ShopNavigator = createDrawerNavigator({
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
+}, {
+    contentOptions: {
+        activeTintColor: Colors.primary
+    }
+})
+
+export default createAppContainer(ShopNavigator);
